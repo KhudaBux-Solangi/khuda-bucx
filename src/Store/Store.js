b6efe5fe9fff76d5f80e774f4e058eced0db8b29
Khuda Bux <khudabux.xwave@gmail.com>
2025-01-27 11:25:50 +0000

    first commit

--- src/Store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import locationReducer from "../Slices/Value";
-
-export const store = configureStore({
-  reducer: {
-  
-    Location: locationReducer,
-  },
-});
